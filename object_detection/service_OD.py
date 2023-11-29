f066cd84c9471b319fcd25767f4b965fae2ccb7f
an-eliseev <eliseev.a@team.karpov.courses>
2023-11-29 20:46:39 +0300

    fix

--- object_detection/service_OD.py
+++ object_detection/service_OD.py
@@ -1,14 +1,9 @@
-# Импортируем класс, от которого будем наследоваться
 from service import Service
-
-# Ниже импорты специфичные для задачи
 import cv2
+from cam import Camera
 
 # Need pip install
 import yolov5
-
-# Импорт класса для работы с камерой
-from cam import Camera
 
 class ServiceOD(Service):
 
@@ -34,52 +29,26 @@ class ServiceOD(Service):
 					continue
 
 				# Получение кадров из потока
-				frame_raw = cap.getFrame()
+				self._frame = cap.getFrame()
 				# Проверка, что кадр непустой
-				if frame_raw is None:
+				if self._frame is None:
 					continue
-
-				# На текущий момент фрейм - это две картинки с RGB-сенсора и со стереопары (Depth) w=1280px, h=480px
-				# RGB фрейм слева, т.е. его координаты (x=0, y=0, w=640, h=480)
-				# Depth фрейм cghfdf, т.е. его координаты (x=640, y=0, w=640, h=480)
-				h, w, ch = frame_raw.shape
-				w = w//2
-				self.frame = frame_raw[:, :]  # получение RGB
-				# frame = frame_raw[0:h, w:2*w]  # получение Depth
 
 				# Обработка кадра (или иная работа сервиса)
 				# В данном примере работа - это определение лица и его идентификация между кадрами
 				result = self.__specific_work()
-				 
-				# Пример коммуникации с другим Сервисом Б
-				# 1) Сервис Б должен прослушивать server_ip:server_port
-				# 2) Сервис Б должен уметь обрабатывать команду req (str-type) и возвращать какой-либо результат
-				# чтобы обработать результат следует использовать функцию, которая принимает ответ сервиса (str)
-				if not result:
-					server_ip = '0.0.0.0'
-					server_port = 0000
-					req = 'no_frame'
-					self.run_client(ip=server_ip, port=server_port, request=req, response_handler=self.__resp_hand)
 				
 				# Пример реализации небольшой задержки и корректного выхода их бесконечного цикла
 				if cv2.waitKey(5) & 0xFF == ord('q'):
 					break
 		
-		finally:    
+		finally:
 			cv2.destroyAllWindows()
-			# close job + server
 			self.stop()
 
 
 	# Вспомогательная функция
-	def __init_vars(self):
-		self._in_target_frames = 0
-		self._total_frames = 0
-
-		self._face_rect = None
-		self._target = None
-		self._target_image = None
-				
+	def __init_vars(self):			
 		self.model = yolov5.load('yolov5s.pt')
 		# set model parameters
 		self.model.conf = 0.25  # NMS confidence threshold
@@ -105,16 +74,13 @@ class ServiceOD(Service):
 				self.model.names[i] = 'alive'
 
 	def __resp_hand(self, response):
-		if response == "goodbye":
-			self._target= None
-		if response == 'hello':
-			self._in_target_frames = 0
+		pass
 
 	# Вспомогательная функция
 	def __specific_work(self):
-		print('load YOLO...')
-		results = self.model(self.frame)
-		return ('alive' in str(results.tolist()[0]))
+		results = self.model(self._frame)
+		print(results.pandas())
+		return None
 
 	def _request_handler(self, request):
 		# Завести набор команд, которые может обрабатывать сервер
@@ -126,9 +92,13 @@ class ServiceOD(Service):
 
 		# ЧТО НУЖНО РЕАЛИЗОВАТЬ
 		# request `__is_alive_here`, который просто будет возвращать есть ли кто-то живой в кадре???????????
-		if request == 'is_alive_here':
-			output = self._do_job()
+		if request == 'checkAlive':
+			results = self.model(self._frame)
+			return ('alive' in str(results.tolist()[0]))
 		
+		if request == 'getFrame':
+			pass
+	
 		# остальной набор команд специфичен для каждого сервиса
 		# данная функция ВСЕГДА должна что-то возвращать либо результат, либо статус (OK, FAILED, etc)
 		some_string = "0"
